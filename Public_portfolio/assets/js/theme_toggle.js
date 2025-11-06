355b5a59e80818a41a115a1370989383ec49c095
HJWAVESHEATH <hjermsbr@gmail.com>
2025-11-06 12:10:34 -0600

    Delete Public_portfolio directory
    Signed-off-by: HJWAVESHEATH <hjermsbr@gmail.com>

--- Public_portfolio/assets/js/theme_toggle.js
+++
@@ -1,5 +0,0 @@
-const toggle = document.getElementById("themeToggle");
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  toggle.textContent = document.body.classList.contains("dark") ? "🌙" : "☀️";
-});
